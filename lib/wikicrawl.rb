3f8fc94e3a711b22314af9ed03b58f9fddf31fe7
Spencer Williams <s@spencerenglish.com>
2015-01-15 11:02:48 -0800

    better crawl method

--- lib/wikicrawl.rb
+++ lib/wikicrawl.rb
@@ -3,13 +3,36 @@ require 'json'
 
 DEBUG = false
 
+def removeTokens(text, startString, stopString, replace)
+  while text != nil && text.index(startString)
+    startIndex = text.index(startString)
+    stopIndex = text[startIndex,text.length-startIndex].index(stopString) + startIndex
+    inner = text[startIndex+startString.length, stopIndex-startIndex-startString.length]
+    # replace can be "true" for "use the token's inner string", or it can be a string to replace the whole token with
+    replaceString = replace===false ? "" : (replace===true ? inner : replace)
+    # puts "inner #{inner}, replace string: #{replaceString}"
+    text[startIndex, stopIndex-startIndex+stopString.length] = replaceString
+  end
+  text
+end
+
 # takes in raw content string for event
 # returns presentable html
 def processEvent(e)
+  if (e.index("&ndash;") == nil)
+    return {"year" => "", "text" => ""}
+  end
+  
   year = e.split("&ndash;").first.strip
   year = year.split("|").last.tr_s("[","").tr_s("]","")
+
+  text = e.split("&ndash;").drop(1).join.strip
   
-  text = e.split("&ndash;").last.strip
+  text = removeTokens(text, "<ref", ">", false)
+  text = removeTokens(text, "</ref", ">", false)
+  text = removeTokens(text, "{{'", "}}", "'")
+  
+  # remove [[foo|bar]] tokens
   while text.index("[[")
     startIndex = text.index("[[")
     stopIndex = text.index("]]")
@@ -17,14 +40,21 @@ def processEvent(e)
     token = token.split("|").last
     text[startIndex, stopIndex-startIndex+2] = token
   end
+  
+  text = removeTokens(text, "<!--", "-->", false)
+  #TODO: do something about {foo|bar|qaz} tokens
+  #TODO: <sub> and <sup>
+  #TODO: decapitalize the first word if it is an article (or if it wasn't originally in a token?!)
+  #TODO: change tenses of verbs...
+  
+  text = text.strip
+  
   return {"year" => year, "text" => text}
   
-  # NLP ideas:
-  # other idea: if first word is not a proper noun, decapitalize it
-  # change tenses of verbs...
 end
 
 def fetch(pageName)
+  puts "fetch #{pageName}"
   response = Wikipedia.find(pageName);
   json = JSON.parse(response.json);
   pages = json["query"]["pages"];
